fad93e373f019405f15cc42d434caf81b95ca03a
Bruno <bruno.silva.castilho@gmail.com>
2025-03-31 11:40:03 -0300

    feat: create database schema

---
+++ db/migrations/20250331143111_create.ts
@@ -0,0 +1,22 @@
+import type { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('meals', (table) => {
+    table.uuid('id').primary()
+    table.text('name').notNullable()
+    table.text('description')
+    table.datetime('date').notNullable()
+    table.boolean('is_on_the_diet').notNullable()
+
+    table
+      .uuid('user_id')
+      .notNullable()
+      .references('id')
+      .inTable('users')
+      .onDelete('CASCADE')
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('meals')
+}
